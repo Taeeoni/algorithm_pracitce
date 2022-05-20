7728dcf37606ee691f4caf7c68fc851d1ebdb9a1
taeeon <taeynheejin@naver.com>
2022-05-20 14:57:35 +0900

    revise

--- Algorithm_test/src/programmers/NumberAddress.java
+++ Algorithm_test/src/programmers/NumberAddress.java
@@ -2,42 +2,23 @@ package programmers;
 
 import java.util.*;
 
-public class NumberAddress {
-
-	public static void main(String[] args) {
-		String s = "aabbaccc";
-		int answer_now = 0;
-		int answer = 0;
-		String slash;
-		List<String> list = new ArrayList<String>();
-		for (int i = 1; i < s.length(); i++) {
-
-			for (int j = 0; j < s.length(); j += i) {
-
-				if (i + j > s.length()) {
-					list.add(s.substring(0 + j, s.length()));
-				} else {
-					list.add(s.substring(0 + j, i + j));
-				}
-
-			}
-			
-			answer_now = s.length() + list.size();
-			for (int j = 0; j < list.size() - 1; j++) {
-				
-				if (list.get(j).equals(list.get(j + 1))) {
-					answer_now -= i;
-				} else {
-					answer_now -= 1;
-				}
-				
-			}
-			answer = Math.min(answer_now, (s.length() + list.size()));
-			System.out.println(answer);
-			list.removeAll(list);
-		}
-		
-		System.out.println(answer);
-	}
-
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap <String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i], i);
+        }
+        
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    answer = false;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
 }
---
+++ Algorithm_test/src/programmers/confress_practice.java
@@ -0,0 +1,45 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class confress_practice {
+
+	public static void main(String[] args) {
+		String s = "aabbaccc";
+		int answer_now = 0;
+		int answer = 0;
+		String slash;
+		List<String> list = new ArrayList<String>();
+		for (int i = 1; i < s.length(); i++) {
+
+			for (int j = 0; j < s.length(); j += i) {
+
+				if (i + j > s.length()) {
+					list.add(s.substring(0 + j, s.length()));
+				} else {
+					list.add(s.substring(0 + j, i + j));
+				}
+
+			}
+			
+			answer_now = s.length() + list.size();
+			for (int j = 0; j < list.size() - 1; j++) {
+				
+				if (list.get(j).equals(list.get(j + 1))) {
+					answer_now -= i;
+				} else {
+					answer_now -= 1;
+				}
+				
+			}
+			answer = Math.min(answer_now, (s.length() + list.size()));
+			System.out.println(answer);
+			list.removeAll(list);
+		}
+		
+		System.out.println(answer);
+
+	}
+
+}
